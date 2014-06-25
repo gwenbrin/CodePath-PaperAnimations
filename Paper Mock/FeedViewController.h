4ff9bf3485d69dc019432927bd9d0d10c4788215
Gwen Brinsmead <gwen@Gwens-MacBook-Pro.local>
2014-06-24 21:28:25 -0700

    added gi image

---
+++ Paper Mock/FeedViewController.h
@@ -0,0 +1,13 @@
+//
+//  FeedViewController.h
+//  Paper Mock
+//
+//  Created by Gwen Brinsmead on 6/22/14.
+//  Copyright (c) 2014 Gwen Brinsmead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FeedViewController : UIViewController
+
+@end
---
+++ Paper Mock/FeedViewController.m
@@ -0,0 +1,38 @@
+//
+//  FeedViewController.m
+//  Paper Mock
+//
+//  Created by Gwen Brinsmead on 6/22/14.
+//  Copyright (c) 2014 Gwen Brinsmead. All rights reserved.
+//
+
+#import "FeedViewController.h"
+
+@interface FeedViewController ()
+
+@end
+
+@implementation FeedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

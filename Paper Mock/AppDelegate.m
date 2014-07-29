5b082483448c2ec425462c99e64bf5ad92f43713
Gwen <gwenbrin@gmail.com>
2014-07-28 17:02:02 -0700

    added missing files

--- Paper Mock/AppDelegate.m
+++ Paper Mock/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "FeedViewController.h"
 
 @implementation AppDelegate
 
@@ -14,6 +15,10 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    
+    FeedViewController *feedViewController = [[FeedViewController alloc] init];
+    self.window.rootViewController = feedViewController;
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
--- Paper Mock/FeedViewController.m
+++ Paper Mock/FeedViewController.m
@@ -10,6 +10,10 @@
 
 @interface FeedViewController ()
 
+@property UIView *feedView;
+@property (assign, nonatomic) CGPoint offsetFromCenter;
+
+]
 @end
 
 @implementation FeedViewController
@@ -21,12 +25,66 @@
         // Custom initialization
     }
     return self;
+    
+    
+    
 }
 
 - (void)viewDidLoad
 {
+    // remove status bar
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    if ([self respondsToSelector:@selector(setNeedsStatusBarAppearanceUpdate)])
+    {
+        [self prefersStatusBarHidden];
+        [self performSelector:@selector(setNeedsStatusBarAppearanceUpdate)];
+    }
+    else
+    {
+        // iOS 6
+        [[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationSlide];
+    }
+    
+    // create frame and imageview for menu
+    CGRect menuFrame = CGRectMake(0, 0, 320, 568);
+    UIImage *menuImage = [UIImage imageNamed:@"menu"];
+    UIImageView *menuImageView = [[UIImageView alloc] initWithFrame:menuFrame];
+    menuImageView.image = menuImage;
+    [self.view addSubview:menuImageView];
+    
+    // create feed view (will contain headline and news)
+    CGRect feedFrame = CGRectMake(0, 0, 320, 568);
+    UIView *feedView = [[UIView alloc] initWithFrame:feedFrame];
+    [self.view addSubview:feedView];
+    
+    // create image view for headline image, add to feed view
+    CGRect headlineFrame = CGRectMake(0, 0, 320, 568);
+    UIImage *headlineImage = [UIImage imageNamed:@"headline"];
+    UIImageView *headlineImageView = [[UIImageView alloc] initWithFrame:headlineFrame];
+    headlineImageView.image = headlineImage;
+    [feedView addSubview:headlineImageView];
+    
+    // create scroll view for news feed, add scroll view to feed view
+    CGRect newsScrollFrame  = CGRectMake(0, 315, 320, 253);
+    UIScrollView *newsScrollView = [[UIScrollView alloc] initWithFrame:newsScrollFrame];
+    newsScrollView.contentSize = CGSizeMake(1444,253);
+    [feedView addSubview:newsScrollView];
+    
+    // create image view for news image, add image view to scroll view
+    CGRect newsFrame = CGRectMake(0, 0, 1444, 253);
+    UIImage *newsImage = [UIImage imageNamed:@"news"];
+    UIImageView *newsImageView = [[UIImageView alloc] initWithFrame:newsFrame];
+    newsImageView.image = newsImage;
+    [newsScrollView addSubview:newsImageView];
+    
+    // create pan gesture recognizer, add to feed view
+    UIPanGestureRecognizer *feedPanGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onFeedPan:)];
+    [feedView addGestureRecognizer:feedPanGestureRecognizer];
+    
+    // assign feedView to feedView property
+    self.feedView = feedView;
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +93,67 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
+- (void)onFeedPan:(UIPanGestureRecognizer *)feedPanGestureRecognizer {
+    
+    CGPoint point = [feedPanGestureRecognizer locationInView:self.view];
+    static CGPoint initialPoint;
+    
+    if (feedPanGestureRecognizer.state == UIGestureRecognizerStateBegan) {
+        NSLog(@"Gesture began at: %@", NSStringFromCGPoint(point));
+        
+        // calculate offset
+        self.offsetFromCenter = CGPointMake(point.x - self.feedView.center.x, point.y - self.feedView.center.y);
+        
+        // get initial y point
+        initialPoint = feedPanGestureRecognizer.self.view.center;
+        NSLog(@"initialPoint: %@", NSStringFromCGPoint(initialPoint));
+
+    } else if (feedPanGestureRecognizer.state == UIGestureRecognizerStateChanged) {
+        NSLog(@"Gesture changed: %@", NSStringFromCGPoint(point));
+        
+        //reposition feedView based on pan
+        if (self.feedView.center.y < 284) {
+            NSLog(@"dragging up beyond the bounds oh noes: %f", point.y);
+            self.feedView.center = CGPointMake(self.feedView.center.x, point.y - self.offsetFromCenter.y);
+        } else if (self.feedView.center.y >= 284) {
+            NSLog(@"dragging within bounds");
+            self.feedView.center = CGPointMake(self.feedView.center.x, point.y - self.offsetFromCenter.y);
+        }
+        
+    } else if (feedPanGestureRecognizer.state == UIGestureRecognizerStateEnded) {
+        NSLog(@"Gesture ended: %@", NSStringFromCGPoint(point));
+        
+        if (initialPoint.y < self.feedView.center.y) {
+            
+            // animate re-positioning feedview on down
+            [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:1.4 initialSpringVelocity:3 options:0 animations:^{
+                self.feedView.center = CGPointMake(self.feedView.center.x, 852 - 44);
+            }completion:nil];
+            
+        } else if (initialPoint.y > self.feedView.center.y) {
+            
+            // animate re-positioning feedview on up
+            [UIView animateWithDuration:1 delay:0 usingSpringWithDamping:1.4 initialSpringVelocity:3 options:0 animations:^{
+                self.feedView.center = CGPointMake(self.feedView.center.x, 284);
+            }completion:nil];
+            
+        }
+        
+    }
+    
+}
+
 @end
+
+
+
+
+
+
+
+
+
